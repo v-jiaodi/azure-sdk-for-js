1ca1be77b5d42c7a21af4548f96d3b0b82b0a029
Di Jiao <v-jiaodi@microsoft.com>
2024-02-02 10:12:15 +0800

    test package

---
+++ sdk/purview/purview-datamap-rest/test/public/testPackage.spec.ts
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert } from "chai";
+import { Context } from "mocha";
+import { DefaultAzureCredential } from "@azure/identity";
+import fs from "fs";
+import PurviewDataMapClientFc, { PurviewDataMapClient, createFileFromStream } from "../../src/index";
+
+
+describe("test package", () => {
+  let client: PurviewDataMapClient;
+
+  beforeEach(async function (this: Context) {
+    client = PurviewDataMapClientFc("https://localhost:3000", new DefaultAzureCredential());
+  });
+
+  // afterEach(async function () {
+  //   await client.stop();
+  // });
+
+  it.only("Upload from a Node stream", async function () {
+    try {
+      const response = await client.path("/atlas/v2/entity/businessmetadata/import").post({
+        contentType: "multipart/form-data",
+        body: {
+          file: createFileFromStream(() => fs.createReadStream("file.txt"), "file.txt"),
+        }
+      });
+      console.log(response)
+    } catch (err) {
+      assert.fail(err as string)
+    }
+  });
+});
